7fbeb5e79d646e1c395bbf638c6b2e5311ede3a9
Yuhang(Steven) Wang <stevenaura@live.com>
2016-11-17 15:54:08 -0600

    add all

---
+++ src/MessagePack.tcl
@@ -0,0 +1,12 @@
+## Module: MessagePack
+#  convert Tcl dict into a MessagePack object
+# -----------------------------------------------
+# Author: Yuhang(Steven) Wang
+# Date: 01/17/2016
+# License: MIT/X11
+# -----------------------------------------------
+
+namespace eval ::MessagePack {
+    namespace export
+    variable DATA
+}
